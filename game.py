08c63a50ca0579e5da16b6c40dd8123c950578c5
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2022-05-07 23:53:23 +0300

    Commented the code.

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 import random
 import copy
 
+# The main game constants. Modify these to change the game.
 BOMB_NUMBER = 50
 BOARD_LENGTH = 20
+
+# The code below creates the board list. Covered block is "X ". Bomb is "B ". Empty block is ". ".
+# "! " is a marked block meaning the player thinks there is a bomb at that location. Blocks with numbers
+# show how many bombs are around.
 shown_board = []
 for board_row in range(0, BOARD_LENGTH):
     if board_row > len(shown_board) - 1:
@@ -13,6 +18,8 @@ for board_row in range(0, BOARD_LENGTH):
 
 
 def range_finder(coord):
+    # A range finder function was needed because looking at adjacent blocks returns index error when near
+    # edges of the board.
     if coord == 0:
         return range(coord, coord+2)
     elif coord == BOARD_LENGTH - 1:
@@ -22,6 +29,8 @@ def range_finder(coord):
 
 class Game:
     def __init__(self):
+        # Hidden board is the real list of board where we hold the information. The player aims to reveal the hidden
+        # board without revealing any bombs.
         self.hidden_board = copy.deepcopy(shown_board)
         self.game_is_on: True
         for i in range(0, BOMB_NUMBER):
@@ -31,6 +40,7 @@ class Game:
         self.mark_count = 0
 
     def plant_bomb(self):
+        # Plants bombs at random at the initialization of the game.
         x = random.randint(0, BOARD_LENGTH - 1)
         y = random.randint(0, BOARD_LENGTH - 1)
         if self.hidden_board[x][y] == "B ":
@@ -38,6 +48,7 @@ class Game:
         self.hidden_board[x][y] = "B "
 
     def set_numbers(self):
+        # After the bombs planted this function runs to look at adjacent blocks to determine what number to put.
         for r in range(0, BOARD_LENGTH):
             for c in range(0, BOARD_LENGTH):
                 if self.hidden_board[r][c] != "B ":
@@ -52,6 +63,8 @@ class Game:
                         self.hidden_board[r][c] = ". "
 
     def player_reveal_move(self, player_input):
+        # When player chooses to reveal a block this function runs. Empty blocks propagate to adjacent empty blocks
+        # to chain reveal. When a bomb is revealed the game ends(return False).
         self.reveal(player_input[0], player_input[1])
         while len(self.propagation_list) > 0:
             for item in self.propagation_list:
@@ -62,6 +75,7 @@ class Game:
         return True
     
     def reveal(self, x, y):
+        # This does the actual revealing and propagation list appending. player_reveal_move uses this function
         shown_board[x][y] = self.hidden_board[x][y]
         if self.hidden_board[x][y] == ". ":
             for row in range_finder(x):
@@ -72,6 +86,9 @@ class Game:
                     shown_board[row][colum] = self.hidden_board[row][colum]
 
     def mark_bomb(self, player_input):
+        # The function to mark blocks. If the number of marks is the same as number of bombs this function
+        # checks for win condition. If every block with mark has a bomb underneath and no other type of block
+        # has any mark then the player has won.
         shown_board[player_input[0]][player_input[1]] = "! "
         self.mark_count += 1
         if self.mark_count == BOMB_NUMBER:
@@ -81,4 +98,4 @@ class Game:
                         return True
 
     def unmark(self, player_input):
-        shown_board[player_input[0]][player_input[1]] = ". "
+        shown_board[player_input[0]][player_input[1]] = "X "
--- minesweeper.py
+++ minesweeper.py
@@ -2,6 +2,7 @@ from game import Game, shown_board
 
 
 def print_board(board):
+    # Turns the list of the board into a string to print it.
     board_string = ""
     for r in board:
         for c in r:
@@ -10,6 +11,7 @@ def print_board(board):
 
 
 def take_coordinates():
+    # Takes input from the player, formats it, returns a list as an output.
     player_input = input("Please enter the coordinates as 'x, y': ")
     input_list = player_input.split(",")
     for i in range(0, 2):
@@ -20,6 +22,7 @@ def take_coordinates():
 my_game = Game()
 game_is_on = True
 while game_is_on:
+    # The main game loop.
     print_board(shown_board)
     player_choice = input("Type 'reveal' to reveal a block, 'mark' to mark a block as bombed, 'unmark' to erase a mark")
     if player_choice == "reveal":
@@ -32,4 +35,5 @@ while game_is_on:
         my_game.unmark(take_coordinates())
     else:
         print("Entered unknown command.")
+# The final print of the board.
 print_board(my_game.hidden_board)
